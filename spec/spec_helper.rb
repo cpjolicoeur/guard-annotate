4a18e430a816f25e25363dc32ae62cda6ac69a39
Cezary Baginski <cezary@chronomantic.net>
2015-01-16 00:22:55 +0100

    remove outdated spec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,8 @@ RSpec.configure do |c|
   c.color_enabled = true
   c.filter_run :focus => true
   c.run_all_when_everything_filtered = true
-  
+
   c.before( :each ) do
-    ENV["GUARD_ENV"] = "test"
     ::Guard::Notifier.stub( :notify ).and_return( true )
-    @lib_path = Pathname.new( File.expand_path( '../../lib/', __FILE__ ) )
-  end
-  
-  c.after( :each ) do
-    ENV["GUARD_ENV"] = nil
   end
 end
